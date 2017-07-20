ba45c85889e3e98bc11622ec568ecb2e96561cfa
Hugh Gormley <sniperplays4days@gmail.com>
2017-07-20 20:30:51 +0000

    added root url

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,8 +1,19 @@
 class TodoController < ApplicationController
-    def index
+    def index 
     end
+    
     def show
-        @todo=Todo.find_by_id (params[:id])
-
+        @todo = Todo.find_by_id(params[:id])
+    end
+    
+    def edit
+        @todo = Todo.find_by_id(params[:id])
+    end
+    def update 
+        t = Todo.find_by_id(params[:id]) 
+        t.description = params['description'] 
+        t.pomodoro_estimate = params['pomodoro-estimate'] 
+        t.save 
+        redirect_to "/todo/show/#{t.id}" 
     end
 end

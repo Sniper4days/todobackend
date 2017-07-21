51b7e6d2b4292867ee0001cf0404d109dbdf0e7e
Hugh Gormley <sniperplays4days@gmail.com>
2017-07-21 14:17:29 +0000

    last day of camp stuff

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,5 +1,6 @@
 class TodoController < ApplicationController
-    def index 
+    def index
+        @todos = Todo.all 
     end
     
     def show
@@ -9,11 +10,26 @@ class TodoController < ApplicationController
     def edit
         @todo = Todo.find_by_id(params[:id])
     end
+    
     def update 
         t = Todo.find_by_id(params[:id]) 
         t.description = params['description'] 
         t.pomodoro_estimate = params['pomodoro-estimate'] 
         t.save 
         redirect_to "/todo/show/#{t.id}" 
+    end
+    
+    def create 
+        t = Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
+    
+    def destroy
+        t = Todo.find_by_id(params[:id])
+        t.destroy
+        redirect_to "/"
     end
 end
